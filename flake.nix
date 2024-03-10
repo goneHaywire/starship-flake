5c3bb5a54151a6a547c5f99f54c9d56c74d53b0f
Emiljan Dusha <emiljanthegreat@gmail.com>
2024-03-10 18:04:25 +0100

    first commit

---
+++ flake.nix
@@ -0,0 +1,15 @@
+{
+  description = "My personal starship flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system};
+      in {
+          packages.default = pkgs.starship;
+      });
+}
